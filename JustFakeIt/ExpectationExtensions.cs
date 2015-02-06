df49de49a50f4be82128df5718dd1bed94ec687a
tom.harris <tom.harris@just-eat.com>
2015-02-06 10:59:01 +0000

    Add the ability to ignore parameters this comes in handy when you have code sending times as part of the query string.

--- JustFakeIt/ExpectationExtensions.cs
+++ JustFakeIt/ExpectationExtensions.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace JustFakeIt
 {
     public static class ExpectationExtensions
     {
+        public const string IgnoreParameter = "{ignore}";
+
         public static bool MatchesActualPath(this HttpRequestExpectation expected, string actualPath)
         {
-            return actualPath.Equals(expected.Url);
+            var newPath = MatchIgnoredParameters(expected, actualPath);
+            return newPath.Equals(expected.Url);
+        }
+
+        private static string MatchIgnoredParameters(HttpRequestExpectation expected, string actualPath)
+        {
+            if (expected.Url.IndexOf(IgnoreParameter, StringComparison.Ordinal) == -1) return actualPath;
+
+            var segments = Regex.Split(expected.Url, IgnoreParameter);
+            for (var i = 0; i < segments.Length; i++)
+            {
+                if (segments[i] == "") continue;
+                var segmentEnd = actualPath.IndexOf(segments[i], StringComparison.Ordinal) + segments[i].Length;
+                var nextSegmentStart = actualPath.Length;
+                if (segments.Length != i + 1)
+                {
+                    if (segments[i + 1] != "")
+                    {
+                        nextSegmentStart = actualPath.IndexOf(segments[i + 1], StringComparison.Ordinal);
+                    }
+                }
+
+                actualPath = actualPath.Replace(actualPath.Substring(segmentEnd, nextSegmentStart - segmentEnd),
+                    IgnoreParameter);
+            }
+            return actualPath;
         }
 
         public static bool MatchesActualHttpMethod(this HttpRequestExpectation expected, string actualHttpMethod)
